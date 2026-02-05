d9658df186d6fb0500ab06ebe79a8ac297d4fbc5
Miguel Angel Ruiz <99372164+aruiz2807@users.noreply.github.com>
2026-02-05 12:21:20 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'super@admin.com',
+            'password' => bcrypt('ld19M7sY3FzE'),
         ]);
     }
 }
